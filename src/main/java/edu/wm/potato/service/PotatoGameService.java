2402e45786cc92d148ac4fe370b3b601ea41632c
jeche <jlchen@email.wm.edu>
2013-12-15 03:19:53 -0500

    pikmans

--- src/main/java/edu/wm/potato/service/PotatoGameService.java
+++ src/main/java/edu/wm/potato/service/PotatoGameService.java
@@ -12,16 +12,19 @@ import edu.wm.potato.Constants;
 import edu.wm.potato.dao.IGameDAO;
 import edu.wm.potato.dao.IPlayerDAO;
 import edu.wm.potato.dao.IPotatoDAO;
+import edu.wm.potato.dao.IPotatoUserDAO;
 import edu.wm.potato.model.GPSLocation;
 import edu.wm.potato.model.Game;
 import edu.wm.potato.model.Player;
 import edu.wm.potato.model.Potato;
+import edu.wm.potato.model.PotatoUser;
 
 public class PotatoGameService {
 	@Autowired IPlayerDAO playerDAO;
 	@Autowired IGameDAO gameDAO;
 	@Autowired PotatoLobbyService lobbyService;
 	@Autowired IPotatoDAO potatoDAO;
+	@Autowired IPotatoUserDAO userDAO;
 
 	public Game remove(String playerID) {
 		Player player = playerDAO.getPlayerById(playerID);
@@ -33,10 +36,38 @@ public class PotatoGameService {
 		if(player.isHasString()) {
 			for(Player p : game.getPlayers()) {
 				p.setScore(p.getScore() + 1 );
+				p.setScore(p.getScore() + 1 );
+				if(!p.isOut()) {
+					PotatoUser user = userDAO.getUserByUsername(p.getUserId());
+					user.setScore(user.getScore() + p.getScore());
+					p.setScore(0);
+					userDAO.update(user);
+				}
 				playerDAO.update(p);
 			}
 		}
 		gameDAO.updateGame(game);
+		playerDAO.update(player);
+		return game;
+	}
+	
+	
+	//Revisit this method to think about the former doing things
+	public Game updatePotatoInfo(String gameId, int score, String potatoId, String playerId, String hadPotato, double lat, double lng) {
+		Game game = gameDAO.getGameById(gameId);
+		Potato potato = potatoDAO.getPotatoById(potatoId);
+		double [] d = new double[2];
+		d[0] = lat;
+		d[1] = lng;
+		Player player = playerDAO.getPlayerById(playerId);
+		Player former = playerDAO.getPlayerById(hadPotato);
+		if(!player.equals(former)) {
+			player.setScore(player.getScore() + score);
+			player.setLat(lat);
+			player.setLng(lng);
+			former.setHasString(false);
+			player.setScore(player.getScore() + score);
+		}
 		playerDAO.update(player);
 		return game;
 	}
